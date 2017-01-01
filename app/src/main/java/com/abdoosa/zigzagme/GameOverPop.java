e1b83bb67b6e0260e8f50f1b7dfc99322e4be767
AbdooSalem <saidfikr@gmail.com>
2017-01-01 14:51:06 +0300

    Updated by Abdoo

---
+++ app/src/main/java/com/abdoosa/zigzagme/GameOverPop.java
@@ -0,0 +1,22 @@
+package com.abdoosa.zigzagme;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+public class GameOverPop extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // DisplayScoreAndShare the layout for the game over pop screen
+        setContentView(R.layout.gameoverpop);
+            }
+
+}
